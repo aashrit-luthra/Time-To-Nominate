24112fe839e9c4519585defd7355b3a29cf65608
Aashrit Luthra <aashrit@bolt.com>
2021-05-12 22:27:33 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
 import './App.css';
 import React, { useState, useEffect } from "react"
-import SearchPage from './Components/SearchPage';
-import Nominations from './Components/SearchPage';
+import SearchBar from "material-ui-search-bar"
+import MovieCard from './Components/MovieCard'
 require('dotenv').config()
 
-
-let nominationList = []
-
-
-
-
-
 function App() {
+  const [nominations, setNominations] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [movieList, setMovieList] = useState({});
+
+  const fetchData = () => {
+    let s = searchQuery.split(" ").join("+");
+    const APICall = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${s}&type=movie`
+    fetch(APICall)
+      .then(response => response.json())
+      .then(data => {
+        setMovieList(data)
+      });
+  }
+
+  const isNominated = (id) => {
+    for (let i = 0; i < nominations.length; i++) {
+      if (nominations[i].imdbID === id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   return (
     <>
-      <SearchPage />
+      {/*  NOMINATIONS */}
+      <h1 className="textstyle" style={{textAlign: "center"}}>Your Nominations: {nominations.length}</h1>
+      {nominations.map((movie) => {
+        return (
+          <div className="card">
+            <MovieCard {...movie}/>
+            <button
+              id='remove-button' 
+              onClick={() => {
+                setNominations(nominations.filter((m) => m.imdbID !== movie.imdbID))
+              }}
+            >Remove</button>
+          </div>
+        )
+      })}
+
+      <hr></hr>
+
+      {/* SEARCH BAR */}
+      <h2 style={{textAlign: "center", color: "#0074D9"}}>Search The Movie Library</h2>
+      <SearchBar
+        value={searchQuery}
+        onChange={(newQuery) => setSearchQuery(newQuery)}
+        onRequestSearch={fetchData}
+      />
+
+      {(Object.keys(movieList).length === 0 || movieList.Response === 'False' || movieList.Search.length === 0) &&
+        <h5 style={{textAlign: "center"}}>No Results Found.</h5>}
+
+      {(Object.keys(movieList).length !== 0 && movieList.Response === 'True' && movieList.Search.length > 0) &&
+        movieList.Search.map((movie) => {
+          return (
+            <div className="card">
+              <MovieCard {...movie} />
+              <button
+                id='nominate-button'
+                onClick={() => { nominations.length < 5 && setNominations(nominations.concat(movie)) }}
+                disabled={nominations.length === 5 || isNominated(movie.imdbID)}
+              >Nominate</button>
+            </div>
+          )
+
+        }
+        )}
+
+        {nominations.length === 5 && alert("You have now entered the maximum number of nominations.")}
+
     </>
   );
 }
--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
+import './MovieCard.css'
 
 export function MovieCard({Title, Year, Poster}) {
     return (
         <>
             <img src={Poster}></img>
-            {Title} - {Year}
+            <h3 id="title">{Title}</h3>
+            <h5 id="year">({Year})</h5>
         </>
     );
 }
+
+export default MovieCard;
